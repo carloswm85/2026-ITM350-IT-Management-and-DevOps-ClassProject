a859af2c8f0dea2a87df60713ac32a900d2d7e1a
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2026-03-24 16:44:37 -0300

    fix # correct type of resource creation

--- iac/main.tf
+++ iac/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-unique-bucket-name-12345"
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-created-instance"
+  }
 }
